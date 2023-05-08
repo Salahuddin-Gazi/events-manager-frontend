58ffca72740f67b672067e57d8798f7ddb1178c0
Salahuddin-Gazi <gazisalahuddin@outlook.com>
2023-05-08 21:52:02 +0600

    Image loader update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // images: {
+  //   loader: "cloudinary",
+  //   domains: ["res.cloudinary.com"],
+  //   path: "",
+  // },
   images: {
-    loader: "cloudinary",
     domains: ["res.cloudinary.com"],
-    path: "",
   },
 };
 
