25ae77af1edfc220893c45acb410b70a0e79a288
Salahuddin-Gazi <gazisalahuddin@outlook.com>
2023-05-08 22:01:26 +0600

    EventMap removed

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,7 @@
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "@/components/Layout";
-import EventMap from "@/components/EventMap";
+// import EventMap from "@/components/EventMap";
 import { API_URL } from "@/config";
 import styles from "@/styles/Event.module.css";
 import Link from "next/link";
